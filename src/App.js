934bc702f948bee8005ae65ce9a4cd1bed0c75f4
timothy hoover <bnicholehilliard@gmail.com>
2022-04-27 15:22:57 -0700

    finally figured it out HA

--- src/App.js
+++ src/App.js
@@ -3,49 +3,37 @@ import axios from 'axios';
 import Character from "./components/Character"
 
 
-const App = () => {
-  const [chars, setChars] = useState([])
-  const [page, setPage] = useState(1)
+const App = (props) => {
+
+
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  useEffect(() => {
-    axios.get(`https://swapi.dev/api/people/`)
-    .then(res => {
-      setChars(res.data);
-      console.log(res.data);
-    })
-    .catch(err => {
-      console.error(err);
-    })
-  }, [])
 
-  useEffect(() => {
-    console.log(page)
-  }, [page])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <Character />
-      <div><button onClick={() => {setPage(page+1)}}> ADD TO PAGE </button></div>
-      <div><button onClick={() => {setPage(page-1)}}> REMOVE FROM PAGE </button></div>
-      {
-        chars.map((character, index) =>(
-          <article key={`app-character-map${index}-${character.id}`}>
-            <h1>{character.name}</h1>
-            <p><b>Birth Year:</b> {character.birth_year} </p>
-            <p><b>Height:</b> {character.height} </p>
-            <p><b>Gender: </b> {character.gender} </p>
-          </article>
-        ))
-      }
-    </div>
-  );
+    <Character />
+    // <div className="App" >
+    //   <Character />
+    //   <div><button onClick={() => {setPage(page+1)}}> ADD TO PAGE </button></div>
+    //   <div><button onClick={() => {setPage(page-1)}}> REMOVE FROM PAGE </button></div>
+    //   {
+    //     chars.map((character, index) =>(
+    //       <article key={`app-character-map${index}-${character.id}`}>
+    //         <h1>{character.name}</h1>
+    //         <p><b>Birth Year:</b> {character.birth_year} </p>
+    //         <p><b>Height:</b> {character.height} </p>
+    //         <p><b>Gender: </b> {character.gender} </p>
+    //       </article>
+    //     ))
+    //   }
+    // </div>
+    )
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,55 @@
 // Write your Character component here
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const StyledChar = styled.div`
 
 h1 {
     color: red;
 }
+
+h1, p {
+    text-align: center;
+}
 `
 
-export default function Character({ name, gender, dob, height }) {
-    return (
-    <StyledChar>
+export default function Character(props) {
+    const [chars, setChars] = useState([])
+    const [page, setPage] = useState(1)
 
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/`)
+        .then(res => {
+          setChars(res.data);
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        })
+      }, [])
+    
+      useEffect(() => {
+        console.log(page)
+      }, [page])
+
+
+
+    return (
+<StyledChar>
+
+
+      {
+        chars.map((character, index) =>(
+          <article key={`app-character-map${index}-${character.id}`}>
+            <h1>{character.name}</h1>
+            <p><b>Birth Year:</b> {character.birth_year} </p>
+            <p><b>Height:</b> {character.height} </p>
+            <p><b>Gender: </b> {character.gender} </p>
+          </article>       
+
+        ))
+}
     </StyledChar>
     )
 }
